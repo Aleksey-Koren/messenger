cb3bcb104d340a820e8156ea586924f904387d33
Aliaksei Koran <akoran@scnsoft.com>
2022-07-07 18:20:56 +0300

    feat: processMessages function

--- src/redux/messenger/messengerActions.ts
+++ src/redux/messenger/messengerActions.ts
@@ -267,65 +267,6 @@ export function fetchMessengerStateTF(loggedUserId: string) {
     }
 }
 
-// function processMessages(dispatch: ThunkDispatch<AppState, void, Action>,
-//                          getState: () => AppState,
-//                          newMessages: Message[]) {
-//
-//     const state = getState();
-//     const currentChat = state.messenger.currentChat;
-//     let currentUser = {...state.messenger.user!};
-//     const chats = {...state.messenger.chats};
-//     const messages = {...state.messenger.messages};
-//     const globalUsers = {...state.messenger.globalUsers};
-//
-//     newMessages.forEach(message => {
-//         switch (message.type) {
-//             case MessageType.HELLO:
-//                 if (!chats[message.chat]) {
-//                     chats[message.chat] = {
-//                         id: message.chat,
-//                         title: message.data!,
-//                         confirmed: false,
-//                         unreadMessages: 0
-//                     }
-//                 } else if (chats[message.chat] && message.data) {
-//                     chats[message.chat].title = message.data;
-//                 }
-//                 dispatch(setChats(chats));
-//                 if (currentChat == null) {
-//                     //case when user just create account (current chat null)
-//                     //and was invited in chat
-//                     dispatch(setCurrentChat(message.chat));
-//                 }
-//                 messages[message.chat].push(message);
-//                 break;
-//             case MessageType.whisper:
-//                 messages[message.chat].push(message);
-//                 break;
-//             case MessageType.iam:
-//                 globalUsers[message.sender].titles[message.chat] = message.data!;
-//                 dispatch(setGlobalUsers(globalUsers));
-//                 if (message.sender === currentUser?.id!) {
-//                     currentUser = {...currentUser, title: message.data} as User;
-//                     dispatch(setUser(currentUser))
-//                 }
-//                 messages[message.chat].push(message);
-//                 break;
-//
-//             case MessageType.who:
-//                 dispatch(sendMessage(globalUsers[currentUser!.id].titles[message.chat] || currentUser!.id, MessageType.iam, () => {
-//                 }));
-//                 messages[message.chat].push(message);
-//                 break;
-//             default:
-//                 throw new Error('Unknown message type: ' + message.type);
-//         }
-//
-//         dispatch(setMessages(messages));
-//
-//     })
-// }
-
 export function updateUserTitle(title: string) {
 
     return (dispatch: ThunkDispatch<AppState, any, Action>, getState: () => AppState) => {
